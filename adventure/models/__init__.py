3ca32a6a418f975715fc3e1fa82f68a7ac689640
lamarmeigs <lmeigs@shiftgig.com>
2016-09-12 17:13:12 -0500

    Build application, models, and test modules

---
+++ adventure/models/__init__.py
@@ -0,0 +1,5 @@
+from adventure.models.game import Game
+from adventure.models.item import Item
+from adventure.models.location import Location
+from adventure.models.person import Person
+from adventure.models.player import Player
