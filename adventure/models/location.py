da4616a9faf91fd811898b1484817080ac9034a0
lamarmeigs <lmeigs@shiftgig.com>
2016-09-15 09:13:40 -0500

    Modify location classes to inherit from BaseModel
    Affects classes: Location, Exit, and Direction, which are all now
    considered to be model classes

--- adventure/models/location.py
+++ adventure/models/location.py
@@ -1,37 +1,65 @@
-class Direction:
+from adventure.models.base import BaseModel
+
+
+class Direction(BaseModel):
     """Describes a direction (either cardinal or relative) to place exits."""
 
-    def __init__(self, name, abbrev):
+    def __init__(self, name, abbrev, _identifier=None):
         """Creates a new `Direction` instance.
 
         Arguments:
             name (str): the primary way to references this direction (eg.
                 "north," "southwest," "up")
             abbrev (str): an abbreviation of the name (eg. "n", "sw", "u")
+            _identifier (int): an optional unique identifier
         """
         self.name = name.lower()
         self.abbrev = abbrev.lower()
+        super().__init__(_identifier=_identifier)
+
+    def serialize(self):
+        """Transform this direction into a JSON-serializable dictionary.
+
+        Return:
+            a dictionary representation of self
+        """
+        return self.__dict__
 
 
-class Exit:
+class Exit(BaseModel):
     """Represents a connection from one location to another."""
 
-    def __init__(self, direction, destination):
+    def __init__(self, direction, destination, _identifier=None):
         """Creates a new `Exit` instance.
 
         Arguments:
             direction (`Direction`): the direction from the player where this
                 exit can be found
             destination (`Location`): the location to which this exit leads
+            _identifier (int): an optional unique identifier
         """
         self.direction = direction
         self.destination = destination
+        super().__init__(_identifier=_identifier)
+
+    def serialize(self):
+        """Transform this exit into a JSON-serializable dictionary.
+
+        Return:
+            a dictionary representation of self
+        """
+        return {
+            'direction': self.direction.reference,
+            'destination': self.destination.reference,
+            '_identifier': self._identifier
+        }
 
 
-class Location:
+class Location(BaseModel):
     """A `Location` represents a single area on a map."""
 
-    def __init__(self, name, description, items=None, people=None, exits=None):
+    def __init__(self, name, description, items=None, people=None, exits=None,
+                 _identifier=None):
         """Creates a new `Location` instance.
 
         Arguments:
@@ -43,9 +71,26 @@ class Location:
                 be found at this location
             exits (list | None): an optional list of Exit objects, detailing
                 how to leave this location for another
+            _identifier (int): an optional unique identifier
         """
         self.name = name
         self.description = description
         self.items = items or []
         self.people = people or []
         self.exits = exits or []
+        super().__init__(_identifier=_identifier)
+
+    def serialize(self):
+        """Transform this location object into a JSON-serializable dictionary.
+
+        Return:
+            a dictionary representation of self
+        """
+        return {
+            'name': self.name,
+            'description': self.description,
+            'items': [item.reference for item in self.items],
+            'people': [person.reference for person in self.people],
+            'exits': [exit.reference for exit in self.exits],
+            '_identifier': self._identifier,
+        }
--- tests/models/test_location.py
+++ tests/models/test_location.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
+from unittest.mock import patch
 
+from adventure.models import Item, Person
 from adventure.models.location import Direction, Exit, Location
 
 
@@ -9,21 +11,83 @@ class DirectionTestCase(TestCase):
         self.assertEqual(north.name, 'north')
         self.assertEqual(north.abbrev, 'n')
 
+    def test_init_calls_super_with_identifier(self):
+        with patch('adventure.models.base.BaseModel.__init__') as mock_init:
+            Direction(name='up', abbrev='u', _identifier=7)
+        mock_init.assert_called_once_with(_identifier=7)
+
+    def test_serialize(self):
+        southwest = Direction(name='southwest', abbrev='sw')
+        self.assertEqual(
+            southwest.serialize(),
+            {
+                'name': 'southwest',
+                'abbrev': 'sw',
+                '_identifier': southwest._identifier,
+            }
+        )
+
 
 class ExitTestCase(TestCase):
+    def setUp(self):
+        self.direction = Direction('left', 'l')
+        self.destination = Location('backstage', "It's full of props")
+
     def test_init_sets_parameters(self):
-        left = Direction('left', 'l')
-        backstage = Location('backstage', "It's full of props")
-        stage_left = Exit(direction=left, destination=backstage)
-        self.assertEqual(stage_left.direction, left)
-        self.assertEqual(stage_left.destination, backstage)
+        stage_left = Exit(
+            direction=self.direction,
+            destination=self.destination
+        )
+        self.assertEqual(stage_left.direction, self.direction)
+        self.assertEqual(stage_left.destination, self.destination)
+
+    def test_init_calls_super_with_identifier(self):
+        with patch('adventure.models.base.BaseModel.__init__') as mock_init:
+            Exit(self.direction, self.destination, _identifier=16)
+        mock_init.assert_called_once_with(_identifier=16)
+
+    def test_serialize(self):
+        exit = Exit(
+            direction=self.direction,
+            destination=self.destination
+        )
+        self.assertEqual(
+            exit.serialize(),
+            {
+                'direction': exit.direction.reference,
+                'destination': exit.destination.reference,
+                '_identifier': exit._identifier,
+            }
+        )
 
 
 class LocationTestCase(TestCase):
-    pass
+    def test_serialize(self):
+        some_exit = Exit(
+            Direction('east', 'e'),
+            Location('Outside', "It's painfully bright and full of people")
+        )
+        location = Location(
+            name='Living Room',
+            description="It's a mess",
+            items=[Item('thing')],
+            people=[Person('Bob', "It's Bob", 'unspecified')],
+            exits=[some_exit]
+        )
+        self.assertEqual(
+            location.serialize(),
+            {
+                'name': location.name,
+                'description': location.description,
+                'items': [item.reference for item in location.items],
+                'people': [person.reference for person in location.people],
+                'exits': [exit.reference for exit in location.exits],
+                '_identifier': location._identifier,
+            }
+        )
 
 
-class InitTestCase(LocationTestCase):
+class LocationInitTestCase(TestCase):
     def test_set_parameters(self):
         location = Location(
             name='Dark Cave',
@@ -48,3 +112,8 @@ class InitTestCase(LocationTestCase):
         self.assertEqual(location.items, [])
         self.assertEqual(location.people, [])
         self.assertEqual(location.exits, [])
+
+    def test_call_super_with_identifer(self):
+        with patch('adventure.models.base.BaseModel.__init__') as mock_init:
+            Location('Dungeon', 'Ew, was that a rat?', _identifier=27)
+        mock_init.assert_called_once_with(_identifier=27)
