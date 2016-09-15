a977ddc3a6e7b343ca000c5d8bcc5c7938725eef
lamarmeigs <lmeigs@shiftgig.com>
2016-09-15 10:45:43 -0500

    Create fixtures module to contain common objects

---
+++ adventure/fixtures/directions.py
@@ -0,0 +1,17 @@
+from adventure.models.location import Direction
+
+
+north = Direction(name='north', abbrev='n')
+south = Direction(name='south', abbrev='s')
+east = Direction(name='east', abbrev='e')
+west = Direction(name='west', abbrev='w')
+
+northeast = Direction(name='northeast', abbrev='ne')
+northwest = Direction(name='northwest', abbrev='nw')
+southeast = Direction(name='southeast', abbrev='se')
+southwest = Direction(name='southwest', abbrev='sw')
+
+up = Direction(name='up', abbrev='u')
+down = Direction(name='down', abbrev='d')
+left = Direction(name='left', abbrev='l')
+right = Direction(name='right', abbrev='r')
