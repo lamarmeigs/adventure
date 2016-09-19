5feb100a96bbdfa927a8546a8023bd5147fdbc0e
lamarmeigs <lmeigs@shiftgig.com>
2016-09-19 09:51:44 -0500

    Add player, locations attributes to Game

--- adventure/models/game.py
+++ adventure/models/game.py
@@ -4,17 +4,22 @@ from adventure.models.base import BaseModel
 class Game(BaseModel):
     """Contains data concerning the overall game in progress."""
 
-    def __init__(self, title, start_blurb, _identifier=None):
+    def __init__(self, title, start_blurb, player, locations,
+                 _identifier=None):
         """Creates a new `Game` instance.
 
         Arguments:
             title (str): the title of this game
             start_blurb (str): the opening scroll text to display at the start
                 of a new game
+            player (Player): a player object
+            locations (list): all locations involved with this game
             _identifier (int): an optional unique identifier
         """
         self.title = title
         self.start_blurb = start_blurb
+        self.player = player
+        self.locations = locations
         self.is_over = False
         self.is_won = False
         super().__init__(_identifier=_identifier)
@@ -28,6 +33,8 @@ class Game(BaseModel):
         return {
             'title': self.title,
             'start_blurb': self.start_blurb,
+            'player': self.player.reference,
+            'locations': [location.reference for location in self.locations],
             '_identifier': self._identifier,
         }
 
--- tests/models/test_game.py
+++ tests/models/test_game.py
@@ -2,30 +2,61 @@ from unittest import TestCase
 from unittest.mock import patch
 
 from adventure.models.game import Game
+from adventure.models.location import Location
+from adventure.models.player import Player
 
 
 class GameTestCase(TestCase):
+    def setUp(self):
+        self.locations = [
+            Location('The Beach', 'Sandy'),
+            Location('The Tower', 'Cold'),
+        ]
+        self.player = Player(location=self.locations[0])
+
     def test_serialize(self):
-        game = Game('Serializable Game', 'In the beginning... the end.')
+        game = Game(
+            'Serializable Game',
+            'In the beginning... the end.',
+            self.player,
+            self.locations,
+        )
         self.assertEqual(
             game.serialize(),
             {
                 'title': game.title,
                 'start_blurb': game.start_blurb,
+                'player': game.player.reference,
+                'locations': [l.reference for l in game.locations],
                 '_identifier': game._identifier,
             }
         )
 
     def test_str(self):
-        game = Game('Printable Game', 'Dullest Game Ever', _identifier=99)
+        game = Game(
+            'Printable Game',
+            'Dullest Game Ever',
+            self.player,
+            self.locations,
+            _identifier=99
+        )
         self.assertEqual(str(game), '<Game 99: Printable Game>')
 
 
-class InitTestCase(TestCase):
+class GameInitTestCase(TestCase):
+    def setUp(self):
+        self.locations = [
+            Location('The Beach', 'Sandy'),
+            Location('The Tower', 'Cold'),
+        ]
+        self.player = Player(location=self.locations[0])
+
     def test_set_parameters(self):
         game = Game(
             title='Test Game',
-            start_blurb='It was a dark and stormy night'
+            start_blurb='It was a dark and stormy night',
+            player=self.player,
+            locations=self.locations,
         )
         self.assertEqual(game.title, 'Test Game')
         self.assertEqual(game.start_blurb, 'It was a dark and stormy night')
@@ -33,7 +64,9 @@ class InitTestCase(TestCase):
     def test_default_parameters(self):
         game = Game(
             title='Test Game',
-            start_blurb='It was a dark and stormy night'
+            start_blurb='It was a dark and stormy night',
+            player=self.player,
+            locations=self.locations,
         )
         self.assertFalse(game.is_over)
         self.assertFalse(game.is_won)
@@ -43,6 +76,8 @@ class InitTestCase(TestCase):
             Game(
                 title='Init Test',
                 start_blurb='You wake up. Everything is dark',
+                player=self.player,
+                locations=self.locations,
                 _identifier=9
             )
         mock_init.assert_called_once_with(_identifier=9)
